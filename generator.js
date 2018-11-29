c637669f148aa2177441bf2592f716825fc9e991
matthewcaren <matthew@sonicjunction.com>
2018-11-29 14:29:57 -0800

    work-in-progress: implementing Tonal-based TRQ

--- generator.js
+++ generator.js
@@ -1,31 +1,4 @@
-// start deg, end deg, "tention quotient"
-var relations = [
-    [1, 2, 1],
-    [1, 4, 1],
-    [1, 5, 3],
-    [1, 6, 2],
-
-    [2, 1, -2],
-    [2, 4, 1],
-    [2, 5, 5],
-    [2, 6, -1],
-    
-    [4, 1, -3],
-    [4, 2, -1],
-    [4, 5, 4],
-    [4, 6, 2],
-
-    [5, 1, -5],
-    [5, 2, 2],
-    [5, 4, -2],
-    [5, 6, 3],
-
-    [6, 1, 2],
-    [6, 2, 3],
-    [6, 4, -1],
-    [6, 5, 4]
-];
-
+// https://github.com/danigb/tonal/blob/master/README.md
 var contour = [1, 2, 3, -2, 2, 4, -5];
 
 var currentChord = 1;
@@ -34,44 +7,76 @@ var chordSequence = new Array(contour.length + 1);
 
 chordSequence[0] = currentChord;
 
-// cycles through every contour value, chooses two "best fit" options, and randomly chooses
-// weighted according to proximity to contour value
-for (i = 0; i < contour.length; i++) {
-    let bestDiff = Infinity;
-    let nextBestDiff = Infinity;
-    let weight = 1;
-    let bestIndex;
-    let nextBestIndex = 0;
-
-    for (j = 0; j < relations.length; j++) {
-        if (relations[j][0] == currentChord) {
-            if (Math.abs(relations[j][2] - contour[i]) < bestDiff) {
-                nextBestDiff = bestDiff;
-                bestDiff = Math.abs(relations[j][2] - contour[i]);
-                nextBestIndex = bestIndex;
-                bestIndex = j;
-
-                weight = Math.sqrt(nextBestDiff - bestDiff)/40 + 0.5;
-            }
-        }
-    }
-    
-    if (Math.random() < weight) {
-        currentChord = relations[bestIndex][1];
-    } else {
-        currentChord = relations[nextBestIndex][1];
-    }
-
-    chordSequence[i+1] = currentChord;
-
+function generate() {
+	for (i = 0; i < contour.length; i++) {
+		// TODO write generation algorithm
+	}
 }
 
-printChords();
+function findtrq(chord1, chord2) {
+	// trq = tension/release quotient
+	// integer from -10 to 10
+	// positive = tense, negative = release
+	let trq = 0;
+	
+	let rootInterval = Math.abs(Tonal.Note.chroma(Tonal.Chord.notes(chord1)[0]) - Tonal.Note.chroma(Tonal.Chord.notes(chord2)[0]))%12;
+
+	// change trq based on root interval
+	switch (rootInterval) {
+		case 0:
+			trq += 1;
+			break;
+		case 1:
+			trq += 4;
+			break;
+		case 2:
+			trq += 3;
+			break;
+		case 3:
+			trq += 2;
+			break;
+		case 4:
+			trq += 3;
+			break;
+		case 5:
+			trq += -6;
+			break;
+		case 6:
+			trq += 9;
+			break;
+		case 7:
+			trq += 0;
+			break;
+		case 8:
+			trq += 1;
+			break;
+		case 9:
+			trq += -2;
+			break;
+		case 10:
+			trq += -3;
+			break;
+		case 11:
+			trq += -2;
+			break;
+		default:
+			console.warn("root interval change not understood");
+	}
+
+	// find number of common tones, subtract double that number from trq
+	trq -= (Tonal.Chord.notes(chord1).filter(value => -1 !== Tonal.Chord.notes(chord2).indexOf(value))).length() * 2;
+
+
+	return Math.min(Math.max(trq, -10), 10);
+}
 
 function printChords() {
 	let chordText = "<b>chords:</b><br />";
 	for (i = 0; i < chordSequence.length; i++) {
 		chordText += chordSequence[i] + " chord<br />";
 	}
-    document.getElementById("chords").innerHTML = chordText;
+	document.getElementById("chords").innerHTML = chordText;
 }
+
+
+printChords(generate());
